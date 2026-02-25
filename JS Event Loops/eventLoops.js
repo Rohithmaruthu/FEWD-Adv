c0d4bff86fef90a06c6e442463ed2398a2c30e95
angurohith <angu.rohith.s.149@kalvium.community>
2026-02-25 11:21:09 +0530

    Event loopss

--- JS Event Loops/eventLoops.js
+++ JS Event Loops/eventLoops.js
@@ -16,3 +16,22 @@ console.log("This will execute before the asynchronous code.");
 // other example of asynchronous code:
 // 1. Simulating an API call with a callback function
 // 2. Using Promises to handle asynchronous operations
+
+// Simulating an API call with a callback function
+function fetchData(callback) {
+    setTimeout(() => {
+        const data = { name: "John", age: 30}
+        callback(data);
+    }, 2000
+)
+}
+
+fetchData((data) => {
+    console.log("Data received from API:", data);
+})
+
+// Macrotasks and Microtasks Example:
+console.log("Start");
+Promise.resolve().then(() => console.log("Microtask 1"));
+setTimeout(() => console.log("Macrotask"), 0);
+console.log("End");
