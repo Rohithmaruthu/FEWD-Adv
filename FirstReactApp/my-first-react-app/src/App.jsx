65f0bdb242095fe3914f31f101605633838d5112
angurohith <angu.rohith.s.149@kalvium.community>
2026-03-11 01:33:37 +0530

    react js

---
+++ FirstReactApp/my-first-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App () {
+  return (
+    <div>
+      <h1>Hello, React World</h1>
+      <p>I built this with vite.</p>
+      <button>Click Me</button>
+    </div>
+  );
+}
+export default App;
